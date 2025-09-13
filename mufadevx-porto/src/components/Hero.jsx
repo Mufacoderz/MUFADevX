5aadc471c22bcc4bd99026e3c08308b0b7d55dae
Muhammad <mf9555759@gmail.com>
2025-09-13 19:28:58 +0800

    mm

--- mufadevx-porto/src/components/Hero.jsx
+++ mufadevx-porto/src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
             frameBorder="0"
             width="100%"
             height="100%"
-            className="w-full h-full hidden sm:block"
+            className="w-full h-full hidden sm:block fixed"
           />
     </div>
 
